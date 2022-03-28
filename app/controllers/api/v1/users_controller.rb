3cd8a70a06665369d9d81a6fe22e0dd3d2917de7
Eduardo Souza <edduardo94@gmail.com>
2022-03-28 10:25:02 -0300

    remove unused dependencies from users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -45,27 +45,6 @@ module Api
         end
       end
 
-      def list
-        Users::ListOperation.new.call(list_dependencies) do |op|
-          op.success do |context|
-            render json: context, status: 200
-          end
-
-          op.failure :validate_contract do |failure|
-            contract = Users::ListContract.new.(params)
-            render json: { code: 400,
-                           status: Message.bad_request,
-                           error: contract.errors }, status: 400
-          end
-
-          op.failure do |failure|
-            render json: { code: 404,
-                           status: Message.not_found("Users"),
-                           error: failure }, status: 404
-          end
-        end
-      end
-
       def show
         Users::ShowOperation.new.call(show_dependencies) do |op|
           op.success do |context|
@@ -87,63 +66,7 @@ module Api
         end
       end
 
-      def search
-        Users::SearchOperation.new.call(search_dependencies) do |op|
-          op.success do |context|
-            render json: context, status: 200
-          end
-
-          op.failure :validate_contract do |failure|
-            contract = Users::SearchContract.new.(params)
-            render json: { code: 400,
-                           status: Message.bad_request,
-                           error: contract.errors }, status: 400
-          end
-
-          op.failure do |failure|
-            render json: { code: 404,
-                           status: Message.not_found("Users"),
-                           error: failure }, status: 404
-          end
-        end
-      end
-
-      def destroy
-        Users::DeleteOperation.new.call(delete_dependencies) do |op|
-          op.success do |context|
-            render json: context, status: 200
-          end
-
-          op.failure :validate_contract do |failure|
-            contract = Users::DeleteContract.new.(params)
-            render json: { code: 400,
-                           status: Message.bad_request,
-                           error: contract.errors }, status: 400
-          end
-
-          op.failure do |failure|
-            render json: { code: 404,
-                           status: Message.not_found("note"),
-                           error: failure }, status: 404
-          end
-        end
-      end
-
       private
-
-      def search_dependencies
-        {
-          contract: Users::SearchContract.new.(params),
-          current_user: @current_user,
-        }
-      end
-
-      def list_dependencies
-        {
-          contract: Users::ListContract.new.(params),
-          current_user: @current_user,
-        }
-      end
 
       def create_dependencies
         {
@@ -156,20 +79,6 @@ module Api
         {
           contract: Users::UpdateContract.new.(params),
           current_user: @current_user,
-        }
-      end
-
-      def delete_dependencies
-        {
-          contract: Users::DeleteContract.new.(params),
-          current_user: @current_user,
-        }
-      end
-
-      def free_Users_dependencies
-        {
-          contract: Users::ListContract.new.(params),
-          current_user: User.find_by(id: 1),
         }
       end
 
